af914223bb80983efc701566f58eea2b3a743b9a
Jordon Davidson <jodavidson@mta.ca>
2024-05-23 13:21:44 -0300

    Update JobListing to use the provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:job_timer/providers/job_list_provider.dart';
 import 'package:job_timer/widgets/app_theme_data.dart';
 import 'package:job_timer/widgets/job_form.dart';
 import 'package:job_timer/widgets/job_listing.dart';
+import 'package:provider/provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future main() async {
@@ -21,11 +23,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppThemeData.light,
-      debugShowCheckedModeBanner: false,
-      title: 'Job Timer',
-      home: const HomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => JobListProvider(),
+      child: MaterialApp(
+        theme: AppThemeData.light,
+        debugShowCheckedModeBanner: false,
+        title: 'Job Timer',
+        home: const HomePage(),
+      ),
     );
   }
 }
--- lib/widgets/job_listing.dart
+++ lib/widgets/job_listing.dart
@@ -1,67 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:job_timer/models/database_helper.dart';
 import 'package:job_timer/models/job.dart';
-import 'package:job_timer/repositories/job.dart';
+import 'package:job_timer/providers/job_list_provider.dart';
+import 'package:provider/provider.dart';
 
-class JobListing extends StatefulWidget {
+class JobListing extends StatelessWidget {
   const JobListing({super.key});
 
   @override
-  State<JobListing> createState() => _JobListingState();
-}
-
-class _JobListingState extends State<JobListing> {
-  final jobRepository = JobRepository(DatabaseHelper());
-  Future<List<Job>>? _jobs;
-
-  @override
-  // Initialize the state by calling the superclass initState method and fetching all jobs from the job repository.
-  void initState() {
-    super.initState();
-    _jobs = jobRepository.findAll();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Job>>(
-      future: _jobs,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-          case ConnectionState.waiting:
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-
-          case ConnectionState.active:
-          case ConnectionState.done:
-            if (snapshot.hasData) {
-              final jobs = snapshot.data;
-              if (jobs!.isNotEmpty) {
-                return ListView.builder(
-                    itemCount: jobs.length,
-                    itemBuilder: (context, index) {
-                      Job job = jobs[index];
-                      return ListTile(
-                        title: Text(job.name ?? 'No name'),
-                        trailing: Text('${job.totalTime}'),
-                      );
-                    });
-              } else {
-                return const Center(
-                  child: Text('No jobs found'),
-                );
-              }
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('An error occurred: $snapshot.error'),
-              );
-            }
-        }
+    return Consumer<JobListProvider>(builder: (context, jobList, child) {
+      if (jobList.isLoading) {
         return const Center(
-          child: Text('No jobs found'),
+          child: CircularProgressIndicator(),
         );
-      },
-    );
+      } else {
+        final jobs = jobList.jobs;
+        if (jobs.isNotEmpty) {
+          return ListView.builder(
+              itemCount: jobs.length,
+              itemBuilder: (context, index) {
+                Job job = jobs[index];
+                return ListTile(
+                  title: Text(job.name ?? 'No name'),
+                  trailing: Text('${job.totalTime}'),
+                );
+              });
+        } else {
+          return const Center(
+            child: Text('No jobs found'),
+          );
+        }
+      }
+    });
   }
 }
+// class JobListing extends StatefulWidget {
+//   const JobListing({super.key});
+
+//   @override
+//   State<JobListing> createState() => _JobListingState();
+// }
+
+// class _JobListingState extends State<JobListing> {
+//   final jobRepository = JobRepository(DatabaseHelper());
+//   Future<List<Job>>? _jobs;
+
+//   @override
+//   // Initialize the state by calling the superclass initState method and fetching all jobs from the job repository.
+//   void initState() {
+//     super.initState();
+//     _jobs = jobRepository.findAll();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<List<Job>>(
+//       future: _jobs,
+//       builder: (context, snapshot) {
+//         switch (snapshot.connectionState) {
+//           case ConnectionState.none:
+//           case ConnectionState.waiting:
+//             return const Center(
+//               child: CircularProgressIndicator(),
+//             );
+
+//           case ConnectionState.active:
+//           case ConnectionState.done:
+//             if (snapshot.hasData) {
+//               final jobs = snapshot.data;
+//               if (jobs!.isNotEmpty) {
+//                 return ListView.builder(
+//                     itemCount: jobs.length,
+//                     itemBuilder: (context, index) {
+//                       Job job = jobs[index];
+//                       return ListTile(
+//                         title: Text(job.name ?? 'No name'),
+//                         trailing: Text('${job.totalTime}'),
+//                       );
+//                     });
+//               } else {
+//                 return const Center(
+//                   child: Text('No jobs found'),
+//                 );
+//               }
+//             } else if (snapshot.hasError) {
+//               return Center(
+//                 child: Text('An error occurred: $snapshot.error'),
+//               );
+//             }
+//         }
+//         return const Center(
+//           child: Text('No jobs found'),
+//         );
+//       },
+//     );
+//   }
+// }
