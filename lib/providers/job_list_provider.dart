0d6cc3128c3b0e514b3b37568afb5cac8197c373
Jordon Davidson <jodavidson@mta.ca>
2024-05-23 12:42:11 -0300

    Updgraded dependencies and added getAllJobs to JobListProvider

--- lib/providers/job_list_provider.dart
+++ lib/providers/job_list_provider.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:job_timer/models/database_helper.dart';
 import 'package:job_timer/models/job.dart';
+import 'package:job_timer/repositories/job.dart';
 
 class JobListProvider extends ChangeNotifier {
-  final List<Job> _jobs = [];
+  List<Job> _jobs = [];
+  bool isLoading = false;
 
   List<Job> get jobs => _jobs;
+
+  Future<void> getAllJobs() async {
+    // set loading state
+    isLoading = true;
+    notifyListeners();
+
+    // Load jobs from database.
+    final jobList = await JobRepository(DatabaseHelper()).findAll();
+
+    _jobs = jobList;
+    isLoading = false;
+    notifyListeners();
+  }
 }
