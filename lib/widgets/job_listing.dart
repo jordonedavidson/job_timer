052d882240b972edd1241dd672f6c62c3aa12165
Jordon Davidson <jodavidson@mta.ca>
2024-05-24 09:45:34 -0300

    Convert JobListing back to StatefulWidget so that we can load initial data from the DB

--- lib/widgets/job_listing.dart
+++ lib/widgets/job_listing.dart
@@ -3,8 +3,21 @@ import 'package:job_timer/models/job.dart';
 import 'package:job_timer/providers/job_list_provider.dart';
 import 'package:provider/provider.dart';
 
-class JobListing extends StatelessWidget {
+class JobListing extends StatefulWidget {
   const JobListing({super.key});
+
+  @override
+  State<JobListing> createState() => _JobListingState();
+}
+
+class _JobListingState extends State<JobListing> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Provider.of<JobListProvider>(context, listen: false).getAllJobs();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
